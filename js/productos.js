63d7ee8a316b74c1464f81718f4b22672d7e2390
Beauty-Patryc <beautypatryc@gmail.com>
2026-08-14 14:03:20 -0500

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -5,7 +5,7 @@ export const productos = [
         nombre: "Kit Romeo", 
         precio: 55000, 
         img: "assets/Kit.png",
-        ingredientes: "Shampoo de Romero, Acondicionador y Tratamiento Capilar(Crema de Peinar).",
+        ingredientes: "Shampoo de Romero para cabello oscuro, Acondicionador y Tratamiento Capilar(Crema de Peinar).",
         aplicacion: "Aplicar el shampoo sobre el cabello húmedo, masajear el cuero cabelludo entre 5 y 15 minutos y enjuagar, luego aplicar acondicionador y tratamiento capilar.",
         duracion: "Aproximadamente 6 meses"
     },
@@ -14,7 +14,7 @@ export const productos = [
         nombre: "Kit Manzanilla", 
         precio: 55000, 
         img: "assets/Kit_manzanilla.png",
-        ingredientes: "Shampoo de Manzanilla, Acondicionador y Tratamiento Capilar(Crema de Peinar).",
+        ingredientes: "Shampoo de Manzanilla para cabello claro, Acondicionador y Tratamiento Capilar(Crema de Peinar).",
         aplicacion: "Aplicar el shampoo sobre el cabello húmedo, masajear el cuero cabelludo entre 5 y 15 minutos y enjuagar, luego aplicar acondicionador y tratamiento capilar.",
         duracion: "Aproximadamente 6 meses"
     },
